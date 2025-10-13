dcbb13a03615ac06e9ef4527739ca58938755397
GabiL <gabriel.loira.lorenzo@gmail.com>
2025-10-13 10:32:23 +0200

    Creamos 2 funciones para depurar el input del jugador y para checkear el estado del combate. Juego incial funciona.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class Main() {
+import java.util.Scanner;
+public class Main {
+    public static boolean vidasCombate(int hpPkm1, int hpPkmn2, String pkm1, String pkm2){
+        boolean estado = false;
+        if (hpPkm1 <= 0 || hpPkmn2 <=0){
+            if (hpPkm1 <= 0){
+                System.out.println(pkm1 + " ha sido derrotado, el ganador es " + pkm2);
+                estado = true;
+            }
+            if (hpPkmn2 <= 0){
+                System.out.println(pkm2 + " ha sido derrotado, el ganador es " + pkm1);
+                estado = true;
+            }
+            return estado;
+        }
+
+        return estado;
+    }
+    public static int depurarInput(Scanner gameInput, int maxValue, int minValue){
+        int numInput = -1; //Inicializamos la variable a -1 para que siempre entre en el loop
+        numInput = gameInput.nextInt();
+        while(!(minValue <= numInput && numInput <= maxValue)){
+            System.out.println("Por favor escribe un valor válido:");
+            numInput = gameInput.nextInt();
+        }
+        return(numInput);
+    }
     public static void main (String[] args){
+        Scanner userInput = new Scanner(System.in);
+        int playerChoice = 0;
+        boolean finCombate = false;
         //Iniciamos el primer pokemon, Pikachu
         String nombre1 = "Pikachu";
         int vida1 = 120;
@@ -21,18 +50,27 @@ public class Main() {
 
         //Combate
         int i = 1;
-        while(i!=0){
+        while(!finCombate){
             System.out.println("Ronda: "+ i);
             System.out.println("Tu Pokemon:");
             System.out.println("Vida: "+vida1);
-            if (vida1 == 0 || vida2 ==0){
-                if (vida1 == 0){
-                    System.out.println(nombre1 + " ha sido derrotado, el ganador es " + nombre2);
-                }
-                if (vida2 == 0){
-                    System.out.println(nombre2 + " ha sido derrotado, el ganador es " + nombre1);
-                }
-                i = 0;
+            System.out.println("Mp: " + mp1);
+            System.out.println("Pokemon enemigo:");
+            System.out.println("Vida: "+vida2);
+            //Combate sección jugador
+            System.out.println("Combate, escribe el número para ejecutar la acción");
+            System.out.println("0: Atacar");
+            playerChoice = depurarInput(userInput, 0,0);
+            if (playerChoice == 0 && !finCombate){
+               vida2 = vida2 - danhoEspecial + defensa2;
+               System.out.println(nombre1 + " le hace "+ (danhoEspecial-defensa2) + " daños a " + nombre2);
+               finCombate = vidasCombate(vida1,vida2, nombre1, nombre2);
+            }
+            //Combate sección máquina
+            if (!finCombate){
+                vida1 = vida1 - danhoFisico + defensa1;
+                System.out.println(nombre2 + " le hace " + (danhoFisico - defensa1) + " daños a "+ nombre1);
+                finCombate = vidasCombate(vida1,vida2, nombre1, nombre2);
             }
         }
     }
