80422751b0b94df98197da6205b90cdb729b31fd
NoamNeo <gabriel.loira.lorenzo@gmail.com>
2025-10-25 11:51:34 +0200

    Trabajando en el nuevo sistema de movimientos

---
+++ src/Move.java
@@ -0,0 +1,11 @@
+public class Move {
+  String nombre;
+  double danho;
+  int mp;
+
+  public Move(String nombre, double danho, int mp) {
+    this.nombre = nombre;
+    this.danho = danho;
+    this.mp = mp;
+  }
+}
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,14 +1,18 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Pokemon {
-    String nombre, nmAtk1, nmAtk2;
-    double vida, mp, danhoAtk1, danhoAtk2, defensaF, defensaS, mpAtk1, mpAtk2;
-    int golpeCrit;
-    double[] pkmAttacks = {danhoAtk1, danhoAtk2};
-    public double ataque(double defensaEnemigo, double vidaEnemigo, boolean crit, int pkmAttack){
-        if(crit){
-            vidaEnemigo = vidaEnemigo - (pkmAttacks[pkmAttack]-defensaEnemigo)*1.5;
-        }else {
-            vidaEnemigo = vidaEnemigo - pkmAttacks[pkmAttack]+defensaEnemigo;
-        }
-        return vidaEnemigo;
+  String nombre, nmAtk1;
+  double vida, mp, danhoAtk1, defensaF, defensaS, mpAtk1;
+  int golpeCrit;
+  Map<String, Move> pkmMoves = new HashMap<>();
+
+  public double ataque(double defensaEnemigo, double vidaEnemigo, boolean crit, double danho) {
+    if (crit) {
+      vidaEnemigo = vidaEnemigo - (danho - defensaEnemigo) * 1.5;
+    } else {
+      vidaEnemigo = vidaEnemigo - danho + defensaEnemigo;
     }
+    return vidaEnemigo;
+  }
 }
